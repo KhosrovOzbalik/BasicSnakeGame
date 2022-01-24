32d49ffbcca32f802707c5dd0ca4119514d732f5
Hüsrev <Mhozbalik@gmail.com>
2022-01-24 12:22:03 +0300

    Snake.cs

---
+++ Assets/Snake.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    Snake tail;
+    bool isDead;
+    Vector2 direction = Vector2.up;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
